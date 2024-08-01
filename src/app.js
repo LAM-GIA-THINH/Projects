13d5d464955fb3d0eb95895ac69845980fc4039f
Thinh <giathinh961617@gmail.com>
2024-08-01 20:43:35 +0700

    chap 1

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const {default: helmet} = require("helmet")
+const compression = require("compression")
+//init middleware
+app.use(morgan("short"))
+app.use(helmet())
+app.use(compression())
+//init db
+
+//init routes
+app.get('/',(req,res,next) => {
+    const StrCom = "Hello"
+    return res.status(200).json({
+        message: 'Welcome',
+        metadata: StrCom.repeat(10000)
+    })
+})
+//init handling error
+
+module.exports = app
